866e9e1e35bf4ae2c9a5a90e39e6b21d10b2cfc3
jisilver <jisilver.kim@gmail.com>
2021-01-20 01:56:39 +0900

    Add login

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 const toggleMenu = document.getElementById('toggle-menu')
-const toggleClose = document.getElementById('toggle-close')
-const banner = document.getElementById('banner')
+const header = document.getElementById('header')
 
 toggleMenu.addEventListener('click', () => {
-    toggleClose.style.display='block'
-    toggleMenu.style.display='none'
-    banner.classList.toggle('show')
-})
-
-toggleClose.addEventListener('click', () => {
-    toggleClose.style.display='none'
-    toggleMenu.style.display='block'
-    banner.classList.remove('show')
+    header.classList.toggle('show')
 })
