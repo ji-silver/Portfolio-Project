d723288b3b4689cbcc965f57f58b53b35437bc6c
jisilver <jisilver.kim@gmail.com>
2021-02-12 01:31:08 +0900

    Add 2018 page & scss

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-const toggleMenu = document.getElementById('toggle')
-const header = document.getElementById('header')
+const header = document.getElementById('header');
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav')
 
-toggleMenu.addEventListener('click', () => {
-    header.classList.toggle('show')
-})
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('open')
+    nav.classList.toggle('open')
+});
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('sticky', window.scrollY > 0)
+});
