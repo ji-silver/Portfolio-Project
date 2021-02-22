53d03641e154b9ae6ce2dfa14626ee40ff24e8e7
jisilver <jisilver.kim@gmail.com>
2021-02-23 01:15:39 +0900

    Fix js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-const header = document.getElementById('header')
-const menuBtn = document.querySelector('.menu-btn')
-const nav = document.querySelector('.nav')
-
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('open')
-    nav.classList.toggle('open')
-});
+const header = document.getElementById('header');
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 0)
 });
+
+const showMenu = () => {
+    const menuBtn = document.getElementById('menu-btn'),
+    nav = document.getElementById('nav')
+
+    if(menuBtn && nav) {
+        menuBtn.addEventListener('click', ()=> {
+            menuBtn.classList.toggle('open')
+            nav.classList.toggle('open')
+        })
+    }
+};
+
+showMenu();
+
+const back = () => {
+    const backBtn = document.getElementById('back-btn')
+
+    if(backBtn) {
+        backBtn.addEventListener('click', ()=> {
+            window.history.go(-1)
+        }) 
+    }
+};
+
+back();
